6c7f0c8707cb21f5f1aa3b3fdc4853d2beb93551
Kanishka Mitra <ankhsaki@gmail.com>
2022-07-07 19:42:58 -0500

    Added fonts, and basic interface

--- MI_visual_interface.py
+++ MI_visual_interface.py
@@ -8,10 +8,16 @@ pygame.display.set_caption("BMI/Harmony: MI Visual Interface")
 
 clock = pygame.time.Clock()
 
-test_surface = pygame.Surface((100,100))
-test_surface.fill('Red')
+#test_surface = pygame.Surface((100,100))
+#test_surface.fill('Red')
 
-pygame.draw.rect(screen,'BLUE',(500,50,200,200),2)
+#Text: subject id 
+base_font = pygame.font.Font('Gilroy-Light.otf', 16)
+user_text_1 = 'Subject ID'
+user_text_2 = 'Session: #'
+user_text_3 = 'Trial: #'
+user_text_4 = 'Timer: '
+
 
 while True: 
 	for event in pygame.event.get():
@@ -20,6 +26,29 @@ while True:
 			exit()
 
 	
-	screen.blit(test_surface,(200,100))
+	#red box 
+	#screen.blit(test_surface,(0,0))
+
+	#text box_1
+	text_surface_1 = base_font.render(user_text_1, False, (255,255,255))
+	screen.blit(text_surface_1, (100,100))
+
+	#text box_2
+	text_surface_2 = base_font.render(user_text_2, False, (255,255,255))
+	screen.blit(text_surface_2, (100,120))
+
+	#text box_3
+	text_surface_3 = base_font.render(user_text_3, False, (255,255,255))
+	screen.blit(text_surface_3, (100,140))
+
+	#text box_4
+	text_surface_4 = base_font.render(user_text_4, False, (255,255,255))
+	screen.blit(text_surface_4, (650,100))
+
+	#task bar
+	pygame.draw.rect(screen,'BLUE',(100,200,600,30),2, 5)
+
+
 	pygame.display.update()
 	clock.tick(60)
+
