e01d8612ef69f94c87a1d696046d163aa3010e82
Kanishka Mitra <ankhsaki@gmail.com>
2022-07-25 10:55:26 -0500

    corrected label positions

--- MI_visual_interface_2.py
+++ MI_visual_interface_2.py
@@ -18,6 +18,7 @@ YELLOW = (255, 255, 0)
 pygame.init()
 
 screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+#screen = pygame.display.set_mode((1400,1000))
 pygame.display.set_caption("BMI/Harmony: MI Visual Interface")
 clock = pygame.time.Clock()
 
@@ -55,7 +56,7 @@ sy_pos = screen.get_height()
 cur_x = int(sx_pos/2) 
 cur_y = int(sy_pos/2) 
 angle1 = 4.71
-stop_ang = 4.71
+stop_ang = round(random.uniform(4.25, 4.75), 2)
 pi = 3.14
 
 
@@ -93,18 +94,17 @@ while True:
 	text_surface_6= base_font.render(user_text_3, False, WHITE)
 	screen.blit(text_surface_6, (cur_x-125,100))
 
-
 	#text box_2
 	text_surface_1 = base_font.render('Subject ID: ' + (sys.argv[1] if len(sys.argv) > 1 else ' '), False, (255,255,255))
-	screen.blit(text_surface_1, (sx_pos*(1/4),100))
+	screen.blit(text_surface_1, (sx_pos*(1/5),100))
 
 	#text box_2
 	text_surface_2 = base_font.render('Session: ' + (sys.argv[2] if len(sys.argv) > 1 else ' '), False, (255,255,255))
-	screen.blit(text_surface_2, (sx_pos*(1/4),150))
+	screen.blit(text_surface_2, (sx_pos*(1/5),150))
 
 	#text box_3
 	text_surface_3 = base_font.render('Trial: ' + str(trial_cnt), False, (255,255,255))
-	screen.blit(text_surface_3, (sx_pos*(1/4),200))
+	screen.blit(text_surface_3, (sx_pos*(1/5),200))
 
 	#text box_4
 	text_surface_4 = base_font.render('Timer: ' + str(tot_sec), False, (255,255,255))
@@ -117,18 +117,17 @@ while True:
 	#start MI bar
 	pygame.draw.rect(screen,GREEN,(cur_x,cur_y-250,20,50),0)
 
-	#subject cursor _2
-	pygame.draw.circle(screen, cursor_col, (325*math.cos(angle1)+(cur_x), 325*math.sin(angle1)+(cur_y+100)), 25)
-
-	#stop MI bar
+	#stop MI arc
 	pygame.draw.arc(screen, RED, [int(sx_pos/2)-350,int(sy_pos/2)-250,700,700], (stop_ang), (stop_ang+.05), 50)
 
 	#stop rest bar
 	pygame.draw.rect(screen,YELLOW,(cur_x-20,cur_y-250,20,50),0)
 
+	#subject cursor _2
+	pygame.draw.circle(screen, cursor_col, (323*math.cos(angle1)+(cur_x), 323*math.sin(angle1)+(cur_y+100)), 25)
+
 	
 	#count down timer 
-	
 	if(tot_sec_2 < 5):
 		task_des = str(5-tot_sec_2)
 		task_col = WHITE
@@ -136,18 +135,18 @@ while True:
 	if(tot_sec_2 == 5):
 		task_des = 'GO!'
 		task_col = GREEN
-		#######################START Trigger 2################
+	#after countdown, begin MI, increase x_pos
+	if(tot_sec_2 > 5):
+		#######################START Trigger 2(Begin MI - 100)################
 		if run_once == 0:
 			print(100)
 			run_once = 1
-	#after countdown, begin MI, increase x_pos
-	if(tot_sec_2 > 5):
 		frame_count += 1
 		#move cursor 
 		cursor_col = GREEN
 		#current task instruction 
 		if((angle1-4.71) >= 7.85-(stop_ang+.05)):
-			task_des = "End MI...Rest"
+			task_des = "End MI!"
 			task_col = RED
 			cursor_col = RED
 			#######################START Trigger 3################
@@ -155,18 +154,18 @@ while True:
 				print(500)
 				run_once_2 = 1
 		if((angle1-4.71) < 7.85-(stop_ang+.05)):
-			task_des = "Begin MI"
+			task_des = "Begin MI!"
 			task_col = GREEN
 		if(angle1 <= 10.95):     #updating cursor position
 			angle1 +=0.01;        #Speed of the cursor 
 		if(angle1 == 10.95):		#stoping cursor at end line 
 			angle1 = 10.95
 			#######################START Trigger 4################
-		if(tot_sec == 10):
+		if(tot_sec == 10.5):
 			if run_once_1 == 0:
 				print(900)
 				run_once_1 = 1
-		if(tot_sec > 10):
+		if(tot_sec > 10.5):
 			task_des = "Relax"
 			task_col = YELLOW
 			cursor_col = YELLOW	
@@ -182,12 +181,13 @@ while True:
 			run_once_1 = 0
 			run_once_2 = 0
 			run_once_3 = 0
+			stop_ang = round(random.uniform(4.00, 5.00), 2)
 
 
 	#once space is pressed, start the trial, countdown 
 	
 	if keys[pygame.K_SPACE]:
-		#######################START Trigger 1################
+		#######################START Trigger 1(Fix - 300)################
 		if run_once_0 == 0:
 			print(300)
 			run_once_0 = 1
@@ -207,6 +207,10 @@ while True:
 	#once space is pressed, start countdown 
 	if(start):
 		frame_count_2 +=1
+		#######################START Trigger 1(Fix - 300)################
+		if run_once_0 == 0:
+			print(300)
+			run_once_0 = 1
 
 
 	#timer for the game
