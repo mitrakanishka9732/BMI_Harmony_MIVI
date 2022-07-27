022678c6411d6f6ecb0f7d7947568fe8c7346690
Kanishka Mitra <ankhsaki@gmail.com>
2022-07-27 16:20:02 -0500

    added exp. ppt.

--- MI_visual_interface_2.py
+++ MI_visual_interface_2.py
@@ -22,16 +22,11 @@ screen = pygame.display.set_mode((1400,1000))
 pygame.display.set_caption("BMI/Harmony: MI Visual Interface")
 clock = pygame.time.Clock()
 
-<<<<<<< Updated upstream
 
-#Text and font 
-base_font = pygame.font.Font('Gilroy_Light.otf', 32)
-=======
 #Text and font 
 base_font = pygame.font.Font('Gilroy-Light.otf', 32) 
 #base_font = pygame.font.Font('./visualInterface/Gilroy_Light.otf', 32)
 
->>>>>>> Stashed changes
 #trial count
 trial_cnt = 1
 task_des = "Begin MI"
